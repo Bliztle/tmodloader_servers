e513b3d2c1ad4fd48928648dd4ed6ecf47c37c73
Bliztle <asbjoern.r.e@gmail.com>
2023-09-30 15:58:51 +0200

    added spectator mod

--- worlds/shay.sh
+++ worlds/shay.sh
@@ -6,6 +6,11 @@ export NAME=terraria_server
 export DOCKER_NAME="$NAME"
 export DATA_PATH=/private/servers/terraria/data
 
+### Added mods
+# 2822925665 : Multiplayer Boss Spectate
+export TMOD_AUTODOWNLOAD="$TMOD_AUTODOWNLOAD,2822925665"
+export TMOD_ENABLEDMODS="$TMOD_ENABLEDMODS,2822925665"
+
 export TMOD_MOTD='Welcome to the tmodloader example server!'
 export TMOD_PASS='CorrectHorseBatteryStaple'
 export TMOD_WORLDNAME='MiddleEarth'
